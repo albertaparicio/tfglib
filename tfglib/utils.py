11da3a4fd2ae500cf2bdabcfd3b455d530707f66
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2017-02-06 18:47:32 +0100

    Utils: Add int2pair and Progbar functions

--- tfglib/utils.py
+++ tfglib/utils.py
@@ -17,6 +17,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import division
 
+import sys
+import time
+
 import numpy as np
 
 
@@ -25,6 +28,20 @@ def kronecker_delta(x):
         return 1
     else:
         return 0
+
+
+def int2pair(val):
+    """Convert a signed integer back into a DTW alignment pair
+
+    :param val Integer to be converted
+    :return DTW alignment pair"""
+
+    if val < 0:
+        return 1, 1 - val
+    elif val > 0:
+        return 1 + val, 1
+    else:
+        return 1, 1
 
 
 def apply_context(input_matrix, context_size):
@@ -202,3 +219,123 @@ def s2s_load_weights(model, filepath, offset=0):
                     weight_values[0] = w
             weight_value_tuples += zip(symbolic_weights, weight_values)
         K.batch_set_value(weight_value_tuples)
+
+
+class Progbar(object):
+    """Displays a progress bar.
+
+    This class has been copied from Keras' source code.
+    https://github.com/fchollet/keras/blob/4cd3d284e9/keras/utils/generic_utils.py
+
+    It is licensed under the MIT License
+    https://github.com/fchollet/keras/blob/4cd3d284e9/LICENSE
+
+    # Arguments
+        :param target: Total number of steps expected.
+        :param interval: Minimum visual progress update interval (in seconds).
+    """
+
+    def __init__(self, target, width=30, verbose=1, interval=0.01):
+        self.width = width
+        self.target = target
+        self.sum_values = {}
+        self.unique_values = []
+        self.start = time.time()
+        self.last_update = 0
+        self.interval = interval
+        self.total_width = 0
+        self.seen_so_far = 0
+        self.verbose = verbose
+
+    def update(self, current, values=None, force=False):
+        """Updates the progress bar.
+
+        # Arguments
+            current: Index of current step.
+            values: List of tuples (name, value_for_last_step).
+                The progress bar will display averages for these values.
+            force: Whether to force visual progress update.
+        """
+        values = values or []
+        for k, v in values:
+            if k not in self.sum_values:
+                self.sum_values[k] = [v * (current - self.seen_so_far),
+                                      current - self.seen_so_far]
+                self.unique_values.append(k)
+            else:
+                self.sum_values[k][0] += v * (current - self.seen_so_far)
+                self.sum_values[k][1] += (current - self.seen_so_far)
+        self.seen_so_far = current
+
+        now = time.time()
+        if self.verbose == 1:
+            if not force and (now - self.last_update) < self.interval:
+                return
+
+            prev_total_width = self.total_width
+            sys.stdout.write('\b' * prev_total_width)
+            sys.stdout.write('\r')
+
+            numdigits = int(np.floor(np.log10(self.target))) + 1
+            barstr = '%%%dd/%%%dd [' % (numdigits, numdigits)
+            bar = barstr % (current, self.target)
+            prog = float(current) / self.target
+            prog_width = int(self.width * prog)
+            if prog_width > 0:
+                bar += ('=' * (prog_width - 1))
+                if current < self.target:
+                    bar += '>'
+                else:
+                    bar += '='
+            bar += ('.' * (self.width - prog_width))
+            bar += ']'
+            sys.stdout.write(bar)
+            self.total_width = len(bar)
+
+            if current:
+                time_per_unit = (now - self.start) / current
+            else:
+                time_per_unit = 0
+            eta = time_per_unit * (self.target - current)
+            info = ''
+            if current < self.target:
+                info += ' - ETA: %ds' % eta
+            else:
+                info += ' - %ds' % (now - self.start)
+            for k in self.unique_values:
+                info += ' - %s:' % k
+                if isinstance(self.sum_values[k], list):
+                    avg = self.sum_values[k][0] / max(1, self.sum_values[k][1])
+                    if abs(avg) > 1e-3:
+                        info += ' %.4f' % avg
+                    else:
+                        info += ' %.4e' % avg
+                else:
+                    info += ' %s' % self.sum_values[k]
+
+            self.total_width += len(info)
+            if prev_total_width > self.total_width:
+                info += ((prev_total_width - self.total_width) * ' ')
+
+            sys.stdout.write(info)
+            sys.stdout.flush()
+
+            if current >= self.target:
+                sys.stdout.write('\n')
+
+        if self.verbose == 2:
+            if current >= self.target:
+                info = '%ds' % (now - self.start)
+                for k in self.unique_values:
+                    info += ' - %s:' % k
+                    avg = self.sum_values[k][0] / max(1, self.sum_values[k][1])
+                    if avg > 1e-3:
+                        info += ' %.4f' % avg
+                    else:
+                        info += ' %.4e' % avg
+                sys.stdout.write(info + "\n")
+
+        self.last_update = now
+
+    def add(self, n, values=None):
+        self.update(self.seen_so_far + n, values)
